337dfd24ef143a0655514a0e34cb333fbcc5e99e
Leon Dail <leondailofficial@gmail.com>
2019-10-01 08:37:54 -0700

    finished second drill

--- src/drills.js
+++ src/drills.js
@@ -7,11 +7,28 @@ const knexInstance = knex({
   connection: process.env.DB_URL,
 });
 
-function showTerm(term) {
-  knexInstance('shopping_list')
-    .select('*')
-    .where('name', 'ILIKE',`%${term}%`)
-    .then(res => console.log(res));
-}
+//Drill #1
+// function showTerm(term) {
+//   knexInstance('shopping_list')
+//     .select('*')
+//     .where('name', 'ILIKE',`%${term}%`)
+//     .then(res => console.log(res));
+// }
 
-showTerm('chil');
+// showTerm('chil');
+
+
+
+//Drill #2
+
+// function showPage(pageNumber) {
+//   let itemsPerPage = 6;
+//   let offset = (itemsPerPage * pageNumber) - itemsPerPage;
+//   knexInstance('shopping_list')
+//     .select('*')
+//     .limit(itemsPerPage)
+//     .offset(offset)
+//     .then(res => console.log(res));
+// }
+
+// showPage(2);
